bded228b18d9e0fb6d42773f91f84ecb4ff922e7
HlengiweNcube <hlehlencube123@gmail.com>
2026-01-16 15:25:58 +0000

    initial Flask application setup

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+ app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Home Page"
+
+@app.route("/about")
+def about():
+    return "About Page"
+
+@app.route("/projects")
+def projects():
+    return "Projects Page"
+
+@app.route("/skills")
+def skills():
+    return "Skills Page"
+
+@app.route("/contact")
+def contact():
+    return "Contact Page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
